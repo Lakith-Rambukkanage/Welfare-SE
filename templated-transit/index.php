ff8ef4dc66cd73ea8f2f0b5abacbbea5e78c5a9c
D.A. Rathnasiri <46153112+DARathnasiri@users.noreply.github.com>
2019-11-08 13:48:59 +0530

    Initial
    Starting codes

---
+++ templated-transit/index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<!--
+	Transit by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Welfare Organization - Homepage</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+	</head>
+	<body class="landing">
+
+		<!-- Header -->
+		<?php 
+		require('includes/header.html');
+		?>
+		
+		<!-- Banner -->
+			<section id="banner">
+				<h2>Stronger Together</h2>
+				<p>Let there be a bright future for EVERYONE</p>
+				<ul class="actions">
+					<li>
+						<a href="#" class="button big">Sign in</a>
+					</li>
+				</ul>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style1 special">
+				<div class="container">
+					<header class="major">
+						<h2>Events around the world</h2>
+						<p>Walkthrough Charity events and help make them better! </p>
+					</header>
+					<div class="row 150%">
+						<div class="4u 12u$(medium)">
+							<section class="box">
+								<i class="icon big rounded color1 fa-plus-square"></i>
+								<h3>Health</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim quam consectetur quibusdam magni minus aut modi aliquid.</p>
+							</section>
+						</div>
+						<div class="4u 12u$(medium)">
+							<section class="box">
+								<i class="icon big rounded color9 fa-paw"></i>
+								<h3>Animals</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium ullam consequatur repellat debitis maxime.</p>
+							</section>
+						</div>
+						<div class="4u$ 12u$(medium)">
+							<section class="box">
+								<i class="icon big rounded color6 fa-child"></i>
+								<h3>Children</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eaque eveniet, nesciunt molestias. Ipsam, voluptate vero.</p>
+							</section>
+						</div>
+					</div>
+				</div>
+				<div align="center">
+							<ul class="actions">
+								<li>
+								<a href="#" class="button big">More</a>
+								</li>
+							</ul>
+				</div>
+			</section>
+			
+		<!-- Two -->
+			<section id="two" class="wrapper style2 special">
+				<div class="container">
+					<header class="major">
+						<h2>Our Staff</h2>
+						<p>Contact our dedicated volunteers to get to know our organization better</p>
+					</header>
+
+<!-- Load admin accounts from sql to the page with name and contact and prof pic-->
+
+					<section class="profiles">
+						<div class="row">
+							<section class="3u 6u(medium) 12u$(xsmall) profile">
+								<img src="images/profile_placeholder.gif" alt="" />
+								<h4>Lorem ipsum</h4>
+								<p>Lorem ipsum dolor</p>
+							</section>
+							<section class="3u 6u$(medium) 12u$(xsmall) profile">
+								<img src="images/profile_placeholder.gif" alt="" />
+								<h4>Voluptatem dolores</h4>
+								<p>Ullam nihil repudi</p>
+							</section>
+							<section class="3u 6u(medium) 12u$(xsmall) profile">
+								<img src="images/profile_placeholder.gif" alt="" />
+								<h4>Doloremque quo</h4>
+								<p>Harum corrupti quia</p>
+							</section>
+							<section class="3u$ 6u$(medium) 12u$(xsmall) profile">
+								<img src="images/profile_placeholder.gif" alt="" />
+								<h4>Voluptatem dicta</h4>
+								<p>Et natus sapiente</p>
+							</section>
+						</div>
+					</section>
+					<footer>
+						<ul class="actions">
+							<li>
+								<a href="#" class="button big">Contact Volunteer</a>
+							</li>
+						</ul>
+					</footer>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="wrapper style3 special">
+				<div class="container">
+					<header class="major">
+						<h2>Help Us Improve</h2>
+						<p>Let us know how we can be better :)</p>
+					</header>
+				</div>
+				<div class="container 50%">
+					<form action="#" method="post">
+						<div class="row uniform">
+							<div class="6u 12u$(small)">
+								<input name="name" id="name" value="" placeholder="Name" type="text">
+							</div>
+							<div class="6u$ 12u$(small)">
+								<input name="email" id="email" value="" placeholder="Email" type="email">
+							</div>
+							<div class="12u$">
+								<textarea name="message" id="message" placeholder="Message" rows="6"></textarea>
+							</div>
+							<div class="12u$">
+								<ul class="actions">
+									<li><input value="Send Message" class="special big" type="submit"></li>
+								</ul>
+							</div>
+						</div>
+					</form>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<?php 
+			require ('includes/footer.html');
+			?>
+	</body>
+</html>
