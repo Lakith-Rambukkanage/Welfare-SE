358dce2805d953fe7d1dd3fa03688ec26feeab11
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2019-11-09 18:54:22 +0530

    Create header_donor.php

---
+++ templated-transit/includes/header_donor.php
@@ -0,0 +1,11 @@
+<!-- Header -->
+			<header id="header">
+				<h1><a href="index.html" class="button special">Welfare Oraganization</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="index.php">My Donations</a></li>
+						<li><a href="#" class="button special">Log Out</a></li>
+					</ul>
+				</nav>
+			</header>
