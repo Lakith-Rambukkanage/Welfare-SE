1287fb889d11c151788e7ae48ce93e91383e3c55
Aanka25 <asankar.17@cse.mrt.ac.lk>
2019-11-10 03:02:18 +0530

    Update Eventspage.php

--- templated-transit/Eventspage.php
+++ templated-transit/Eventspage.php
@@ -9,7 +9,8 @@
 		<script src="js/jquery.min.js"></script>
 		<script src="js/skel.min.js"></script>
 		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
+        <script src="js/init.js"></script>
+        <script src="https://kit.fontawesome.com/yourcode.js"></script>
 		<noscript>
 			<link rel="stylesheet" href="css/skel.css" />
 			<link rel="stylesheet" href="css/style.css" />
@@ -34,11 +35,11 @@
             // output data of each row
                 ?><div class="name" style="font-family: century gothic"><i class="fa fa-user-circle-o"></i>&nbsp </div><?php
                 while($row = mysqli_fetch_assoc($result_set)) {
-                    ?><div class="text" style="font-family: century gothic">
+                    ?><div class="text1" style="font-family: century gothic">
                     <?php    echo "<span class='usernameClass'>"  .'</span>'."<br>"
-                        ?><i class="   fa fa-star"> &nbsp</i><?php  echo $row["event_name"]."<br>"."<br>"?><?php
-                        ?><div class="text2" style="font-family:Century Gothic;"><i class="fa fa-user-circle-o"> &nbsp<?php  echo $row["org_name"]?></i></div>
-                        <div style="font-size: 15px; display:inline;"><i class="fa fa-calendar" >&nbsp &nbsp</i><?php echo $row["event_type"]  ?></div><?php
+                        ?><i class="   fa fa-star"> &nbsp</i><?php  echo $row["name"]."<br>"."<br>"?><?php
+                        ?><div class="text2" style="font-family:Century Gothic;"><i class="fas fa-money-bill-wave"> &nbsp<?php  echo $row["req_amount"]?></i></div>
+                        <div style="font-size: 15px; display:inline;"><i class="far fa-check-circle" ></i><?php echo $row["type"]  ?></div><?php
                     ?></div><?php
                     ?><div class="hr"><hr></div><?php
 
@@ -56,9 +57,23 @@
 			require ('includes/footer.html');
         ?>
     </body>
+<style>
+.text1{
+    padding:0px;
+    margin-left:35%;
+    width:30%;
+    color:white;
+    height:20%;
+    border:1px solid black;
+    background-color: #445;
+    text-align:center;
+    border-radius:11px
+}
+</style>
 </html>
 <script>
     function Open(){
         window.open("AddEvent.php");
     }
 </script>
+
