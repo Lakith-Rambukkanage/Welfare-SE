18404851f31b772422a053fad1c695952d3371ac
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2019-11-09 20:00:05 +0530

    Update header_donor.php

--- templated-transit/includes/header_donor.php
+++ templated-transit/includes/header_donor.php
@@ -1,10 +1,10 @@
 <!-- Header -->
 			<header id="header">
-				<h1><a href="index.html" class="button special">Welfare Oraganization</a></h1>
+				<h1><a href="homepage_donor.php" class="button special">Welfare Oraganization</a></h1>
 				<nav id="nav">
 					<ul>
-						<li><a href="index.php">Home</a></li>
-						<li><a href="index.php">My Donations</a></li>
+						<li><a href="homepage_donor.php">Home</a></li>
+						<li><a href="homepage_donor.php">My Donations</a></li>
 						<li><a href="#" class="button special">Log Out</a></li>
 					</ul>
 				</nav>
