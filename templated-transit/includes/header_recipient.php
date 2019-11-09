d218eb2eb33bff98d0e585503e80cc92de85ba93
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2019-11-09 20:00:22 +0530

    Create header_recipient.php

---
+++ templated-transit/includes/header_recipient.php
@@ -0,0 +1,13 @@
+<!-- Header -->
+			<header id="header">
+				<h1><a href="homepage_recipient.php" class="button special">Welfare Oraganization</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="homepage_recipient.php">Home</a></li>
+						<li><a href="homepage_recipient.php">+Add Event</a></li>
+						<li><a href="homepage_recipient.php">My Events</a></li>
+						<li><a href="homepage_recipient.php">View Donors</a></li>
+						<li><a href="#" class="button special">Log Out</a></li>
+					</ul>
+				</nav>
+			</header>
