04e15138d2d544c62b69c7b6d386044826cd42e2
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2019-11-10 01:21:45 +0530

    Update homepage_admin.php

--- templated-transit/homepage_admin.php
+++ templated-transit/homepage_admin.php
@@ -73,37 +73,6 @@
 				</div>
 			</section>
 			
-
-		<!-- Three -->
-			<section id="three" class="wrapper style3 special">
-				<div class="container">
-					<header class="major">
-						<h2>Help Us Improve</h2>
-						<p>Let us know how we can be better :)</p>
-					</header>
-				</div>
-				<div class="container 50%">
-					<form action="#" method="post">
-						<div class="row uniform">
-							<div class="6u 12u$(small)">
-								<input name="name" id="name" value="" placeholder="Name" type="text">
-							</div>
-							<div class="6u$ 12u$(small)">
-								<input name="email" id="email" value="" placeholder="Email" type="email">
-							</div>
-							<div class="12u$">
-								<textarea name="message" id="message" placeholder="Message" rows="6"></textarea>
-							</div>
-							<div class="12u$">
-								<ul class="actions">
-									<li><input value="Send Message" class="special big" type="submit"></li>
-								</ul>
-							</div>
-						</div>
-					</form>
-				</div>
-			</section>
-
 		<!-- Footer -->
 			<?php 
 			require ('includes/footer.html');
