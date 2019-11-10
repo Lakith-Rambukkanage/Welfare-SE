09f28c4149d9aaf6d97448886cc010329c1f1e19
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2019-11-10 12:29:06 +0530

    Create header_login.php

---
+++ templated-transit/includes/header_login.php
@@ -0,0 +1,12 @@
+<!-- Header -->
+			<header id="header">
+				<h1><a href="index.html" class="button special">Welfare Oraganization</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="generic.php">Generic</a></li>
+						<li><a href="elements.php">Elements</a></li>
+						<li><a href="signup.php" class="button special">Sign up</a></li>
+					</ul>
+				</nav>
+			</header>
