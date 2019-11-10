a5cacec0bd1e84100a6572afc6c6bd9e1df6cdb1
Yoshani Ranaweera <yoshani.malinka@gmail.com>
2019-11-10 08:38:09 +0530

    events_recipient

--- templated-transit/Eventspage.php
+++ templated-transit/Eventspage.php
@@ -76,11 +76,9 @@
                <h3><?php echo 'You have not added any events yet';?></h3></div>
                </header>
                <?php }
-       
+                require ('includes/footer.html');
 
-       ?>
-                
-    
+                ?>
 
         <!-- <button class="button" onclick="Open()">Add Event</button> -->
         <style>
@@ -88,16 +86,16 @@
             /* background-image: url("images/yellow.jpg"); */
             /* background-size: auto; */
             /* background-attachment: scroll; */
-            background-color: #F6FE0D;
+            background-color: #FEF964;
             }
             *{margin:0px; padding:0px; font-family:Helvetica, Arial, sans-serif;}
-            .container {
+            /* .container {
             text-align:justify;
             border-radius: 5px;
             background-color: #f2f2f29c;
             padding: 50px;
             width: 50%
-            }
+            } */
             .col-sm-4 {
                 text-align:Center;
                 border-radius: 5px;
@@ -148,13 +146,11 @@
 
             
         </style>
-			<?php 
-			// require ('includes/footer.html');
-			?>
+
     </body>
 </html>
-<script>
+<!-- <script>
     function Open(){
         window.open("AddEvent.php");
     }
-</script>
+</script> -->
