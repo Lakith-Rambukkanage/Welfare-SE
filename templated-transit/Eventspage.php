6b29c18d9d05e87b495383e3abe2ad168102d983
Aanka25 <asankar.17@cse.mrt.ac.lk>
2019-11-10 10:52:34 +0530

    Update Eventspage.php

--- templated-transit/Eventspage.php
+++ templated-transit/Eventspage.php
@@ -10,7 +10,7 @@
 		<script src="js/skel.min.js"></script>
 		<script src="js/skel-layers.min.js"></script>
         <script src="js/init.js"></script>
-        <script src="https://kit.fontawesome.com/yourcode.js"></script>
+        <!-- <script src="https://kit.fontawesome.com/yourcode.js"></script> -->
 		<noscript>
 			<link rel="stylesheet" href="css/skel.css" />
 			<link rel="stylesheet" href="css/style.css" />
